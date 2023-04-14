a408e840159a212de5ad5c54978307f7727b73ef
Evan Groopman <evan.groopman@nist.gov>
2023-04-14 09:15:24 -0400

    update changelog to version 0.1.2

--- FCpy/__init__.py
+++ FCpy/__init__.py
@@ -5,7 +5,7 @@ try:
 except ModuleNotFoundError:
     import FCpy.FC as FC
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __pkgname__ = 'FCpy'
 __author__ = 'Evan Groopman'
 __url__ = 'https://github.com/usnistgov/FCpy'
@@ -14,6 +14,13 @@ __copyright__ = ''
 __description__ = __doc__
 
 _changelog = """
+Version 0.1.2:
+    - Add convenience function FC_poisson_confband() for calculating confidence band for a range of n.
+    - Convenience function FC_poisson_list() for calculating a list of n and t values.
+    - Add optional user-defined limits in FC_poisson() for range of mu to calculate (mumin, mumax).
+      This can increase computation speed over the conservative auto bounds.
+      Add warnings for CL_high/CL_low hitting the user-defined mumax/mumin.
+
 Version 0.1.1:
     - Add option to fix minor pathology at fixed n0 with different b.
     - Add optional dask dependency to perform this search faster.
